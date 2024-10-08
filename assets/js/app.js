4f3858eddcdc0ae18249516eac687c09179e642b
abdulhalim100 <abdulhalim500083@gmail.com>
2024-10-09 01:33:22 +0600

    Swiper Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,54 @@
-    var swiper = new Swiper(".mySwiper", {
+    var swiper = new Swiper(".trust-Swiper", {
       loop:true,
       autoplay:true,
       pagination: {
         el: ".swiper-pagination",
         dynamicBullets: true,
       },
-    });
-
-
-
-    var swiper = new Swiper(".saySlider", {
-      
-      loop:true,
-      autoplay:false,
-
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-
       breakpoints: {
-        640: {
+        440: {
           slidesPerView: 1,
-          spaceBetween: 0,
+          spaceBetween: 10,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
         },
         768: {
-          slidesPerView: 1,
-          spaceBetween: 30,
+          slidesPerView: 4,
+          spaceBetween: 40,
         },
         1024: {
-          slidesPerView: 2,
-          spaceBetween: 30,
+          slidesPerView: 6,
+          spaceBetween: 50,
         },
       },
+
     });
-    var swiper = new swiper(".mySwiper", {
-
-      loop:tue,
-      autoplay:tre,
-
+    var swiper = new Swiper(".sayslideritem", {
       slidesPerView: 1,
-      spaceBetween: 100,
+      spaceBetween: 10,
+      centeredSlides: true, // Center the active slide
+      initialSlide: 1,
+      loop:true,
+      autoplay:true,
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
       },
     });
 
